9d38e87f959b6f84895e3fc987fb9a77e1efca44
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-03-28 04:49:44 +0000

    Reviewed submission

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def text_numeral(num: int) -> str:
   str
     The converted number in text numerals
   """
-  NUM_WORD = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eigth', 9:'nine', 10:'ten', 11:'eleven', 12:'tweleve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thrity', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'}
+  NUM_WORD = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eigth', 9:'nine', 10:'ten', 11:'eleven', 12:'tweleve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'}
   outp = ''
   n = 1
   while num != 0:
@@ -22,6 +22,7 @@ def text_numeral(num: int) -> str:
     if num >= largest_NUM_WORD:
       num -= largest_NUM_WORD
       outp += NUM_WORD[largest_NUM_WORD]+' '
-    n += 1
+    else:
+      n += 1
   return outp.strip()
-#print(text_numeral(29))
+#print(text_numeral(1700))
